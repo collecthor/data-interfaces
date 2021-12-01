2505c0584a3b0149cca6bf3d6d4feb2b325a216f
Sam Mousa <sam@mousa.nl>
2021-12-01 10:53:22 +0100

    chore: initial setup

---
+++ ecs.php
@@ -0,0 +1,25 @@
+<?php
+declare(strict_types=1);
+
+// ecs.php
+use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\ValueObject\Option;
+use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
+
+return static function (ContainerConfigurator $containerConfigurator): void {
+    $parameters = $containerConfigurator->parameters();
+    // Parallel
+    $parameters->set(Option::PARALLEL, true);
+// A. full sets
+    $containerConfigurator->import(SetList::PSR_12);
+
+
+
+// B. standalone rule
+    $services = $containerConfigurator->services();
+    $services->set(ArraySyntaxFixer::class)
+        ->call('configure', [[
+            'syntax' => 'short',
+        ]]);
+};
