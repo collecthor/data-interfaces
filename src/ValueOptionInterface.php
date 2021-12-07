7899b195b9fcc818609ff8c362296ef957e948e3
Sam Mousa <sam@mousa.nl>
2021-12-07 12:33:35 +0100

    fix: re-add default locale constant

--- src/ValueOptionInterface.php
+++ src/ValueOptionInterface.php
@@ -5,7 +5,7 @@ namespace Collecthor\DataInterfaces;
 
 interface ValueOptionInterface extends ValueInterface
 {
-
+    public const DEFAULT_LOCALE = 'default';
     public function getRawValue(): string|int|float;
     public function getDisplayValue(null|string $locale = null): string;
 
