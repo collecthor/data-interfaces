89624d5e0da84d2be06dbbfe30ef657f6bf4499e
Sam Mousa <sam@mousa.nl>
2022-07-08 10:59:27 +0200

    fix: update dependency

--- ecs.php
+++ ecs.php
@@ -4,22 +4,17 @@ declare(strict_types=1);
 // ecs.php
 use PhpCsFixer\Fixer\ArrayNotation\ArraySyntaxFixer;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
 use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
-return static function (ContainerConfigurator $containerConfigurator): void {
-    $parameters = $containerConfigurator->parameters();
+return static function (ECSConfig $ECSConfig): void {
     // Parallel
-    $parameters->set(Option::PARALLEL, true);
-// A. full sets
-    $containerConfigurator->import(SetList::PSR_12);
+    $ECSConfig->parallel();
 
+    $ECSConfig->sets([
+        SetList::PSR_12
+    ]);
 
-
-// B. standalone rule
-    $services = $containerConfigurator->services();
-    $services->set(ArraySyntaxFixer::class)
-        ->call('configure', [[
-            'syntax' => 'short',
-        ]]);
+    $ECSConfig->rule(ArraySyntaxFixer::class);
 };
