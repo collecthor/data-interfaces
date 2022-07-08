bcb9af1c4962b56d37c6ddf45d86abca7071d42e
Sam Mousa <sam@mousa.nl>
2022-07-08 13:14:59 +0200

    feat!: add getDisplayValues

--- src/ValueOptionInterface.php
+++ src/ValueOptionInterface.php
@@ -9,4 +9,9 @@ interface ValueOptionInterface extends ValueInterface
     public function getRawValue(): string|int|float;
     public function getDisplayValue(null|string $locale = null): string;
 
+    /**
+     * @return array<string, string> Display values for each available locale, indexed by locale
+     */
+    public function getDisplayValues(): array;
+
 }
