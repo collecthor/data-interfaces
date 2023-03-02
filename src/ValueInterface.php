32e8e80a98c701d49ef863060a20c4bb64a3a72f
Rick Wouters <rick@happytechnology.nl>
2023-03-02 08:54:00 +0100

    feat: Add DateTimeValueInterface and DateTimeVariableInterface

--- src/ValueInterface.php
+++ src/ValueInterface.php
@@ -3,11 +3,12 @@ declare(strict_types=1);
 
 namespace Collecthor\DataInterfaces;
 
+use DateTimeInterface;
 
 interface ValueInterface
 {
     /**
-     * @return string|int|float|bool|array<mixed>|null
+     * @return string|int|float|bool|array<mixed>|null|DateTimeInterface
      */
-    public function getRawValue(): string|int|float|bool|null|array;
+    public function getRawValue(): string|int|float|bool|null|array|DateTimeInterface;
 }
